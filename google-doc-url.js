ba4c805749c082066eba2f77c661a0e3dd3b78eb
Colin Strasser <157181707+CStras@users.noreply.github.com>
2026-04-08 14:58:20 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQGGiJxyt_2x5FmU4Wydkv-hzHRD4GgEmr40Njl4VhzkFD7c9AbfVYR0fjyUcUAYIQ5UUWFUP0q9Y8N/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
